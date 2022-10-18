a16a1abe0936d77d560556fbcb74ab92cb221850
Senthil Kumar P <senthilm784@gmail.com>
2022-10-18 21:37:27 +0530

    First Spotify Clone

---
+++ README.md
@@ -0,0 +1,1 @@
+# Build and Deploy a Better Spotify 2.0 Clone Music App with React 18! (Tailwind, Shazam, Redux)
---
+++ src/components/MusicPlayer/Track.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Track = ({ isPlaying, isActive, activeSong }) => (
+  <div className="flex-1 flex items-center justify-start ">
+    <div className={`${isPlaying && isActive ? 'animate-[spin_3s_linear_infinite]' : ''} hidden sm:block h-16 w-16 mr-4`}>
+      <img src={activeSong?.images?.coverart} alt="cover art" className="rounded-full" />
+    </div>
+    <div className="w-[50%]">
+      <p className="truncate text-white font-bold text-lg sm: w-20">
+        {activeSong?.title ? activeSong?.title : 'No active Song'}
+      </p>
+      <p className="truncate text-gray-300 sm: w-10">
+        {activeSong?.subtitle ? activeSong?.subtitle : 'No active Song'}
+      </p>
+    </div>
+  </div>
+);
+
+export default Track;
